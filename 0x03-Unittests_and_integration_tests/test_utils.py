20a097941f92d4448b3ec54f4524becf5b7f4181
omar Boutoutla <omar_bt@outlook.fr>
2025-07-19 13:27:21 -0700

    unittest

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,9 @@
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    def test_access_nested_map(self):
+        nested_map = {"a": {"b": {"c": 1}}}
+        result = access_nested_map(nested_map, ["a", "b", "c"])
+        self.assertEqual(result, 1)
